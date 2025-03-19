53da79c208282338e5046f302d6fcc137c4f6778
filipkk78 <filipkk78@gmail.com>
2025-03-19 17:32:08 +0100

    Add index view

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 const newRouter = require("./routes/newRouter");
 const indexRouter = require("./routes/indexRouter");
 
 app.use("/new", newRouter);
 app.use("/", indexRouter);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -15,6 +15,6 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("Homepage"));
+indexRouter.get("/", (req, res) => res.render("index", { messages: messages }));
 
 module.exports = indexRouter;
