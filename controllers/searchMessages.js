360de8b027a8aeb78b76ce35fac8f5cb496041e6
filipkk78 <filipkk78@gmail.com>
2025-03-24 21:37:47 +0100

    Remove unnecessary console.log

--- controllers/searchMessages.js
+++ controllers/searchMessages.js
@@ -3,7 +3,6 @@ const db = require("../db/queries");
 async function searchMessages(req, res) {
   const { messageId } = req.params;
   const message = await db.getMessageById(messageId);
-  console.log(message);
   if (!message) {
     res.send("Message with this id hasn't been found");
   } else {
